f16e12cfefd69b13587fcf44e4d6cc8775f1ad6f
Kamrin Klauschie <kamrin.klauschie@gmail.com>
2019-10-14 18:09:37 -0300

    Refactored area calculator function

--- 00-Setup/00-Demo/lib/demo.rb
+++ 00-Setup/00-Demo/lib/demo.rb
@@ -1,9 +1,8 @@
 def circle_area(radius)
   # TODO: Implement the `circle_area` method
   area = Math::PI * (radius ** 2)
-    if radius < 0
+    if radius.negative?
       return 0
-  else
-    return area
-  end
+    else
+      return area
 end
