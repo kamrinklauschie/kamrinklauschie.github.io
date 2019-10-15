891a9fe60ce3d2683f1b467f0172c7b380bb8a0f
Kamrin Klauschie <kamrin.klauschie@gmail.com>
2019-10-15 16:55:55 -0300

    Implemented coaching

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -1,8 +1,25 @@
 def coach_answer(your_message)
   # TODO: return coach answer to your_message
+  if your_message == "I am going to work right now!"
+    return ""
+  elsif your_message.include? "?"
+    return "Silly question, get dressed and go to work!"
+  elsif your_message == your_message.upcase
+    coach_answer_enhanced(your_message)
+  else
+    return "I don't care, get dressed and go to work!"
+  end
 end
 
 def coach_answer_enhanced(your_message)
   # TODO: return coach answer to your_message, with additional custom rules of yours!
+  if your_message == 'I AM GOING TO WORK RIGHT NOW!'
+    return ""
+  elsif your_message.include? "."
+    return "I can feel your motivation! I don't care, get dressed and go to work!"
+  elsif your_message.include? "?"
+    return "I can feel your motivation! Silly question, get dressed and go to work!"
+  else
+    return "I can feel your motivation! I don't care, get dressed and go to work!"
+  end
 end
-
--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,2 +1,14 @@
 require_relative "coach_answer"
 # TODO: Implement the program that makes you discuss with your coach from the terminal.
+
+### Talking with the user ###
+
+while true
+  puts "Welcome to your coaching session!"
+  your_message = gets.chomp
+  the_answer = coach_answer(your_message)
+  p the_answer
+  break if the_answer == ""
+end
+
+#############################
