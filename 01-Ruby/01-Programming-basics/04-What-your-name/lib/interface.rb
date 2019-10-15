60b1be527b5e4143942ff44cf2d15230dea9b9fe
Kamrin Klauschie <kamrin.klauschie@gmail.com>
2019-10-15 14:33:35 -0300

    Implemented string interpolation

--- 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
@@ -6,3 +6,17 @@ require_relative "compute_name"
 # TODO: Print a greeting to the user with the full name concatenated,
 #       something like "Hello, Boris Paillard"
 
+### Talking with the user ###
+puts 'What\'s your first name?'
+first_name = gets.chomp
+
+puts 'What\'s your middle name?'
+middle_name = gets.chomp
+
+puts 'What\'s your last name?'
+last_name = gets.chomp
+#############################
+
+# Print user's full name
+
+puts compute_name(first_name, middle_name, last_name)
