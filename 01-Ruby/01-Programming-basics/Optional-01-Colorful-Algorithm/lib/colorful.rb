9257f431917bee3e4012074558971ffca36baf0f
Kamrin Klauschie <kamrin.klauschie@gmail.com>
2019-10-15 11:03:08 -0300

    Started colorful algorithm

--- 01-Ruby/01-Programming-basics/Optional-01-Colorful-Algorithm/lib/colorful.rb
+++ 01-Ruby/01-Programming-basics/Optional-01-Colorful-Algorithm/lib/colorful.rb
@@ -1,3 +1,28 @@
 def colorful?(number)
   # TODO: return true if the number is colorful, false otherwise
+
+def colorful?(num)
+  for each digit in num
+    x * y, y * z, x * y * z
+  if
+    return true
+  elseif num > 999
+    return
+  else
+    return false
+  end
+end
+
+
+
+
+0 = colorful
+1 = colorful
+23 = colorful
+263 = colorful
+987 = colorful
+10 = not colorful
+236 = not colorful
+999 = not colorful
+
 end
