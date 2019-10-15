dccfa5892ab76ba18d4904d22c7d182d3c5065b6
Kamrin Klauschie <kamrin.klauschie@gmail.com>
2019-10-15 14:16:10 -0300

    Implemented age in days calculator

--- 01-Ruby/01-Programming-basics/03-Age-in-days/lib/age_in_days.rb
+++ 01-Ruby/01-Programming-basics/03-Age-in-days/lib/age_in_days.rb
@@ -1,7 +1,12 @@
 # This "require" line loads the contents of the 'date' file from the standard
 # Ruby library, giving you access to the Date class.
-require 'date'
+require "date"
+require "time"
 
 def age_in_days(day, month, year)
   # TODO: return the age expressed in days given the day, month, and year of birth
+  birthday = Date.new(year, month, day)
+  today = Date.today
+  calculated_age = (today - birthday).to_i
+  return calculated_age
 end
