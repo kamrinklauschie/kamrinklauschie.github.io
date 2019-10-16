6ce4eeaf7662bbb2986756eccaba75def7981a0c
Kamrin Klauschie <kamrin.klauschie@gmail.com>
2019-10-16 10:36:04 -0300

    Implemented voter age conditional

--- 01-Ruby/02-Flow-Array/01-Can-you-vote/lib/vote.rb
+++ 01-Ruby/02-Flow-Array/01-Can-you-vote/lib/vote.rb
@@ -1,4 +1,12 @@
 def allowed_to_vote?(age)
   # TODO: return (not print!) a boolean stating whether `age` is old enough to vote
   # NOTE: Use an if/else statment
+
+puts "What is your age?"
+age = gets.chomp
+
+if age < 18
+  return false
+else
+  return true
 end
