553ce71113f59332b89bd8ad036a27334518838f
Kamrin Klauschie <kamrin.klauschie@gmail.com>
2019-10-16 19:09:44 -0300

    Implemented wagon sort

--- 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/interface.rb
@@ -6,6 +6,29 @@ require_relative "wagon_sort"
 #       - `chomp`: http://www.ruby-doc.org/core-2.5.3/String.html#method-i-chomp
 
 
+names = []
+puts "Type a student name:"
+input = gets.chomp
+names << input
+
+counter = 0
+until input == ""
+  puts "Type another student name or press enter to finish:"
+  input = gets.chomp
+  counter += 1
+  break if input == ""
+
+  names << input
+
+end
 
 # TODO: Then call `wagon_sort` method defined in the wagon_sort.rb
 #       file and display the sorted student list
+string_list = wagon_sort(names)
+
+puts 'debugging'
+p "this is names"
+p names
+
+puts "Congratulations! Your Wagon has #{counter} students:
+#{string_list[0..-2].join(', ')} and #{string_list.last}"
--- 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/wagon_sort.rb
+++ 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/wagon_sort.rb
@@ -1,3 +1,8 @@
 def wagon_sort(students)
   # TODO: return (not print!) a copy of students, sorted alphabetically
+  if students == []
+    return []
+  else
+    students.sort_by { |name| name.capitalize }
+  end
 end
