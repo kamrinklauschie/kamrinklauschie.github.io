1ce2df4eeebb0b757a8cad98849ce2b40cbf7ac1
Kamrin Klauschie <kamrin.klauschie@gmail.com>
2019-10-16 11:04:36 -0300

    Implemented vote calculator

--- 01-Ruby/02-Flow-Array/01-Can-you-vote/lib/vote.rb
+++ 01-Ruby/02-Flow-Array/01-Can-you-vote/lib/vote.rb
@@ -1,12 +1,12 @@
 def allowed_to_vote?(age)
-  # TODO: return (not print!) a boolean stating whether `age` is old enough to vote
-  # NOTE: Use an if/else statment
-
-puts "What is your age?"
-age = gets.chomp
-
-if age < 18
-  return false
-else
-  return true
+  if age >= 18
+    return true
+  else
+    return false
+  end
 end
+
+
+# def allowed_to_vote?(age)
+#  return age >= 18 ? true : false
+# end
