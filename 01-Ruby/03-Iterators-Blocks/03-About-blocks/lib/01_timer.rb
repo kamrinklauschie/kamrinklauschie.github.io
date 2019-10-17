4c2ef0e2884f5791b07ca6f8ec7fc920743cc054
Kamrin Klauschie <kamrin.klauschie@gmail.com>
2019-10-17 16:08:40 -0300

    Implemented blocks challenges

--- 01-Ruby/03-Iterators-Blocks/03-About-blocks/lib/01_timer.rb
+++ 01-Ruby/03-Iterators-Blocks/03-About-blocks/lib/01_timer.rb
@@ -1,3 +1,8 @@
 def timer_for
   # TODO: Return time taken to execute the given block
+  time_start = Time.now
+  yield
+
+  time_end = Time.now
+  return time_end - time_start
 end
--- 01-Ruby/03-Iterators-Blocks/03-About-blocks/lib/03_tag.rb
+++ 01-Ruby/03-Iterators-Blocks/03-About-blocks/lib/03_tag.rb
@@ -1,4 +1,10 @@
 def tag(tag_name, attributes = nil)
   # TODO: Build HTML tags around content given in the block
-  #       The method can be called with an optional HTML attribute inputted as [attr_name, attr_value]
+  # The method can be called with an optional HTML attribute inputted as [attr_name, attr_value]
+  value = yield
+  if attributes.nil?
+    return "<#{tag_name}>#{value}</#{tag_name}>"
+  elsif attributes.count == 2
+    return "<#{tag_name} #{attributes[0]}=\"#{attributes[1]}\">#{value}</#{tag_name}>"
+  end
 end
