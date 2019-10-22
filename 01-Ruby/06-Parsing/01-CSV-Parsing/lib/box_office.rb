68147ce2908a891c245f71d07031759534cfb432
Kamrin Klauschie <kamrin.klauschie@gmail.com>
2019-10-22 15:40:44 -0300

    Implemented movie parsing without style errors

--- 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
+++ 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
@@ -5,11 +5,7 @@ def most_successful(number, max_year, file_path)
   movie_data_array = []
   CSV.foreach(file_path) do |movie_data|
     if movie_data[1].to_i < max_year
-      movie_data_array.push(
-        name: movie_data[0],
-        year: movie_data[1].to_i,
-        earnings: movie_data[2].to_i
-      )
+      movie_data_array.push(name: movie_data[0], year: movie_data[1].to_i, earnings: movie_data[2].to_i)
     end
   end
   movie_data_array.sort_by! { |movie| movie[:earnings] }.reverse!
