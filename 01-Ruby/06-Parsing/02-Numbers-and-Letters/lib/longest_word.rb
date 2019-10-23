a67ca516184acc6371533e9ecf540d4fe471adb8
Kamrin Klauschie <kamrin.klauschie@gmail.com>
2019-10-23 10:46:33 -0300

    Implemented start of game logic

--- 01-Ruby/06-Parsing/02-Numbers-and-Letters/lib/longest_word.rb
+++ 01-Ruby/06-Parsing/02-Numbers-and-Letters/lib/longest_word.rb
@@ -3,8 +3,33 @@ require 'json'
 
 def generate_grid(grid_size)
   # TODO: generate random grid of letters
+  letters = ["a".."z"]
+  $grid = letters.sample(grid_size)
 end
 
 def run_game(attempt, grid, start_time, end_time)
   # TODO: runs the game and return detailed hash of result
+  url = "https://wagon-dictionary.herokuapp.com/#{attempt}"
+  user_serialized = open(url).read
+  api_hash = JSON.parse(user_serialized)
+
+  case error_handling
+  when api_hash["word"].match? $grid
+    p $grid
 end
+
+run_game("kamrin", 6, 0, 0)
+
+# custom message for correct word
+# compute high score for longer word
+# compute high score for quicker answer
+
+
+
+# make sure word entered is an actual English work
+# make sure that every character appears in the grid
+# score 0 for non-English word
+# custom message for invalid word
+# score 0 for word not in the grid
+# custom message for word not in grid
+# catches overused letters
