0c95daf24c52cbb6c4e8feebfb3ffed9ee1d4b4f
Kamrin Klauschie <kamrin.klauschie@gmail.com>
2019-10-23 11:21:08 -0300

    Implemented Restaurant class

---
+++ 02-OOP/01-OO-Basics/01-Define-your-own-class/lib/interface.rb
@@ -0,0 +1,5 @@
+require_relative restaurant.rb
+
+restaurant.new("Bistro 33", "American", "El Dorado Hills")
+restaurant.new("Boomerang Mix", "Brazilian", "Rio de Janeiro")
+bistro_33.open_restaurant(closed)
---
+++ 02-OOP/01-OO-Basics/01-Define-your-own-class/lib/restaurant.rb
@@ -0,0 +1,12 @@
+class Restaurant
+  def initialize(name, food_type, location)
+    @name = name
+    @food_type = food_type
+    @location = location
+    @status = status
+  end
+
+  def open_restaurant(status)
+    status
+  end
+end
