f337e16b5c1e258c9e2ea11df9859eeade32a8e5
Kamrin Klauschie <kamrin.klauschie@gmail.com>
2019-10-23 15:37:22 -0300

    Implemented orange tree functionality

--- 02-OOP/01-OO-Basics/02-Getters-and-Setters/lib/vending_machine.rb
+++ 02-OOP/01-OO-Basics/02-Getters-and-Setters/lib/vending_machine.rb
@@ -1,5 +1,7 @@
 class VendingMachine
   # TODO: add relevant getter/setter to this class to make the scenarios work properly.
+  attr_reader :amount_cents, :snack_price_cents
+  attr_accessor :snacks
 
   def initialize(snack_price_cents, snacks)
     @amount_cents = 0
@@ -10,10 +12,15 @@ class VendingMachine
   def insert_coin(value_cents)
     # TODO: what happens to @snacks, @amount_cents and @snack_price_cents
     # when the user inserts a coin?
+    @amount_cents += value_cents
   end
 
   def buy_snack
     # TODO: what happens to @snacks, @amount_cents and @snack_price_cents
     # when the user pushes a button to buy a Snack?
+    if @snacks.positive? && @snack_price_cents <= @amount_cents
+      @amount_cents = @amount_cents - @snack_price_cents
+      @snacks = @snacks - 1
+    end
   end
 end
--- 02-OOP/01-OO-Basics/03-Basic-OOP/lib/orange_tree.rb
+++ 02-OOP/01-OO-Basics/03-Basic-OOP/lib/orange_tree.rb
@@ -1,3 +1,42 @@
+require 'pry-byebug'
+
 class OrangeTree
+  attr_accessor :age, :height, :fruits, :status
+
   # TODO: Implement all the specs defined in the README.md :)
+  def initialize
+    @age = 0
+    @height = 0
+    @fruits = 0
+    @dead = false
+  end
+
+  def one_year_passes!
+    @age += 1
+    @height += 1 if height < 10
+    fruit_count
+    if @age > 50 && @age < 100
+      @dead = true if rand(1..100) < @age
+    elsif @age >= 100
+      @dead = true
+    end
+  end
+
+  def fruit_count
+    if @age > 5 && @age < 10
+      @fruits = 100
+    elsif @age >= 10 && @age < 15
+      @fruits = 200
+    else
+      @fruits = 0
+    end
+  end
+
+  def dead?
+    @dead
+  end
+
+  def pick_a_fruit!
+    @fruits = @fruits - 1 if @fruits.positive?
+  end
 end
