bf55a6311425d903bd4ee5664ff8a917346001b7
Kamrin Klauschie <kamrin.klauschie@gmail.com>
2019-10-24 15:23:55 -0300

    Implemented animal and winning combo classes

--- 02-OOP/02-OO-Advanced/04-About-self/lib/animal.rb
+++ 02-OOP/02-OO-Advanced/04-About-self/lib/animal.rb
@@ -1,22 +1,26 @@
 class Animal
   def name(name)
     @name = name
+    return self
     # TODO: what should this method return?
   end
 
   def species(species)
     @species = species
     # TODO: what should this method return?
+    return self
   end
 
   def color(color)
     @color = color
     # TODO: what should this method return?
+    return self
   end
 
   def natural_habitat(natural_habitat)
     @natural_habitat = natural_habitat
     # TODO: what should this method return?
+    return self
   end
 
   def to_s
