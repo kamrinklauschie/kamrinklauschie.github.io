bd4045f7386103dc56ef7ef513f5b02bb49867ab
Kamrin Klauschie <kamrin.klauschie@gmail.com>
2019-10-24 12:56:22 -0300

    Implemented city class with class filter method

--- 02-OOP/02-OO-Advanced/03-Instance-vs-Class/lib/restaurant.rb
+++ 02-OOP/02-OO-Advanced/03-Instance-vs-Class/lib/restaurant.rb
@@ -1,9 +1,28 @@
 class Restaurant
   # TODO: add relevant accessors if necessary
 
+  attr_reader :city, :name
+  attr_writer :average_rating
+
   def initialize(city, name)
     # TODO: implement constructor with relevant instance variables
+    @city = city
+    @name = name
+    @average_rating_array = []
+    @average_rating = 0
+  end
+
+  def average_rating
+    @average_rating_array.sum / @average_rating_array.length
   end
 
   # TODO: implement .filter_by_city and #rate methods
+  def rate(new_rate)
+    @average_rating_array << new_rate
+    return average_rating
+  end
+
+  def self.filter_by_city(restaurants, city)
+    return restaurants.select { |restaurant| restaurant.city == city }
+  end
 end
