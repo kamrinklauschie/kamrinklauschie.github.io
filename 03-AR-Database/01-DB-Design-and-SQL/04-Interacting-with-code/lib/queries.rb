4e968746066c1b30c7fbcf465806bf2a88da865a
Kamrin Klauschie <kamrin.klauschie@gmail.com>
2019-10-31 13:34:11 -0300

    Implemented partial queries

--- 03-AR-Database/01-DB-Design-and-SQL/04-Interacting-with-code/lib/queries.rb
+++ 03-AR-Database/01-DB-Design-and-SQL/04-Interacting-with-code/lib/queries.rb
@@ -1,21 +1,35 @@
+require 'pry-byebug'
 require 'sqlite3'
 
 def artist_count(db)
   # TODO: use `db` to execute an SQL query against the database.
+  results = db.execute("SELECT COUNT (*) FROM artists")
+  # Alternative - results.flatten.first
+  results[0][0]
 end
 
 def number_of_rows(db, table_name)
   # TODO: count number of rows in table table_name
+  results = db.execute("SELECT COUNT (*) FROM #{table_name}")
+  results[0][0]
 end
 
 def sorted_artists(db)
   # TODO: return array of artists' names sorted alphabetically
+  results = db.execute("SELECT name FROM artists ORDER BY name ASC")
+  return results.flatten
 end
 
 def love_tracks(db)
   # TODO: return array of love songs' names
+  results = db.execute("SELECT name FROM tracks WHERE name LIKE '%love%'")
+  return results.flatten
 end
 
 def long_tracks(db, min_length)
   # TODO: return an array of tracks' names verifying: duration > min_length (minutes) sorted by length (ascending)
+  results = db.execute("SELECT name FROM tracks
+    WHERE milliseconds >= #{min_length}
+    ORDER BY milliseconds ASC")
+  return results.flatten
 end
