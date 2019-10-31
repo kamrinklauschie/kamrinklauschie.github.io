b0e2fe7f4fd4cb4d8b66878c21a1c69f2a44d0c2
Kamrin Klauschie <kamrin.klauschie@gmail.com>
2019-10-31 17:29:51 -0300

    Implemented partial queries

--- 03-AR-Database/01-DB-Design-and-SQL/05-SQL-queries/lib/join_queries.rb
+++ 03-AR-Database/01-DB-Design-and-SQL/05-SQL-queries/lib/join_queries.rb
@@ -11,10 +11,23 @@ end
 
 def stats_on(db, genre_name)
   # TODO: For the given category of music, return the number of tracks and the average song length (as a stats hash)
-
+  # Find number of tracks for genre
+  # Add length of all songs, divide by number of all songs
+  # Return as a formatted hash
+  stats_array = db.execute("SELECT genres.name,
+    COUNT(tracks.genre_id),
+    ROUND(AVG(tracks.milliseconds)/ 60000, 2)  FROM tracks
+    JOIN genres ON genres.id = tracks.genre_id
+    WHERE genres.name = '#{genre_name}';")
+  transformed_array = stats_array.flatten
+  stats_hash = {
+    category: transformed_array[0],
+    number_of_songs: transformed_array[1],
+    avg_length: transformed_array[2]
+  }
+  return stats_hash
 end
 
 def top_five_artists(db, genre_name)
   # TODO: return list of top 5 artists with the most songs for a given genre.
 end
-
