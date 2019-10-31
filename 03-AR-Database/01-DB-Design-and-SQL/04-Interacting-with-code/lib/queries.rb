a76c0d6f2678f43e170f644154b3d014438f193e
Kamrin Klauschie <kamrin.klauschie@gmail.com>
2019-10-31 15:15:05 -0300

    Corrected style errors

--- 03-AR-Database/01-DB-Design-and-SQL/04-Interacting-with-code/lib/queries.rb
+++ 03-AR-Database/01-DB-Design-and-SQL/04-Interacting-with-code/lib/queries.rb
@@ -28,7 +28,7 @@ end
 
 def long_tracks(db, min_length)
   # TODO: return an array of tracks' names verifying: duration > min_length (minutes) sorted by length (ascending)
-  length = min_length * 60000
+  length = min_length * 60_000
   results = db.execute("SELECT name FROM tracks
     WHERE milliseconds >= #{length}
     ORDER BY milliseconds ASC")
