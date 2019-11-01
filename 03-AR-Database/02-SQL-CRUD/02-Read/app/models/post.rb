27ae90364ef1b4a86a0d41397301714e7b489ef2
Kamrin Klauschie <kamrin.klauschie@gmail.com>
2019-11-01 13:14:37 -0300

    Implemented partial queries

--- 03-AR-Database/02-SQL-CRUD/02-Read/app/models/post.rb
+++ 03-AR-Database/02-SQL-CRUD/02-Read/app/models/post.rb
@@ -4,4 +4,32 @@
 
 class Post
   # TODO
+  attr_reader :votes, :id
+  attr_accessor :title, :url
+  def initialize(attributes = {})
+    @title = attributes[:title]
+    @id = attributes[:id]
+    @url = attributes[:url]
+    @votes = attributes[:votes]
+  end
+
+  def self.find(id)
+    results = db.execute("SELECT * FROM table WHERE (?, ?)", [id, #{id}])
+    return results
+  end
+
+  def self.all
+    results = db.execute('SELECT * FROM posts;')
+  end
 end
+
+
+# CREATE TABLE `posts` (
+#   `id`  INTEGER PRIMARY KEY AUTOINCREMENT,
+#   `title` TEXT,
+#   `url` TEXT,
+#   `votes`  INTEGER
+# );
+
+
+# db.execute("INSERT INTO player_info (touchdowns, team) VALUES (?, ?)", [10, "Giants"]
