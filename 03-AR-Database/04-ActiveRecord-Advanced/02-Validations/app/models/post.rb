52a1cd6c51bb583cb9a816ea171f0f897a5cd503
Kamrin Klauschie <kamrin.klauschie@gmail.com>
2019-11-05 17:42:57 -0300

    Implemented user and post validation

--- 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/post.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/post.rb
@@ -1,8 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-
-  # TODO: Add some validation
   validates :user, presence: true
-  validates :name, presence: true, length: { minimum: 5 }
+  validates :name, presence: true, length: { minimum: 5 }, uniqueness: { case_sensitive: false }
   validates :url, presence: true, format: { with: /https?:\/\/[\S]+/ }
 end
--- 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
@@ -5,4 +5,11 @@ class User < ActiveRecord::Base
   validates :username, uniqueness: true, presence: true
   validates :email, uniqueness: true, presence: true, format: { with: /\A.*@.*\.com\z/ }
   # TODO: Add some callbacks
+  before_validation :strip_email
+
+  private
+
+  def strip_email
+    self.email = email.strip unless email.nil?
+  end
 end
