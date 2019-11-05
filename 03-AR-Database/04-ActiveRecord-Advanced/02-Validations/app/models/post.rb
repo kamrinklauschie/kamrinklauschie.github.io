5eebff17a7b6485c4dcf9993a82eb4b8587441ab
Kamrin Klauschie <kamrin.klauschie@gmail.com>
2019-11-05 17:19:48 -0300

    Implemented partial User and Post model with validation

--- 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/post.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ActiveRecord::Base
   belongs_to :user
 
   # TODO: Add some validation
+  validates :user, presence: true
+  validates :name, presence: true, length: { minimum: 5 }
+  validates :url, presence: true, format: { with: /https?:\/\/[\S]+/ }
 end
--- 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
@@ -2,5 +2,7 @@ class User < ActiveRecord::Base
   has_many :posts
 
   # TODO: Add some validation
+  validates :username, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true, format: { with: /\A.*@.*\.com\z/ }
   # TODO: Add some callbacks
 end
