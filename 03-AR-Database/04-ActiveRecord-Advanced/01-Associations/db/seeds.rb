f60d6e246717d99792ffda6511350b3d80ad8525
Kamrin Klauschie <kamrin.klauschie@gmail.com>
2019-11-05 13:09:37 -0300

    Added users model and seed

--- 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/seeds.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/seeds.rb
@@ -1,1 +1,19 @@
-# TODO: Write a seed
+require "faker"
+
+5.times do
+  user = User.new(
+    username: Faker::Internet.username,
+    email: Faker::Internet.email
+  )
+  user.save!
+
+  10.times do
+    post = Post.new(
+      name: Faker::Quote.yoda,
+      url: Faker::Internet.url,
+      votes: rand(1..10),
+      user: user
+    )
+    post.save!
+  end
+end
