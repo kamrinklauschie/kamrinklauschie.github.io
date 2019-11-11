9d57db01fc5185cfcd797629941f93f3590da75f
Kamrin Klauschie <kamrin.klauschie@gmail.com>
2019-11-11 12:35:14 -0300

    Implemented function

--- 04-Front-End/04-JavaScript-basics/01-Even-or-odd/lib/even_or_odd.js
+++ 04-Front-End/04-JavaScript-basics/01-Even-or-odd/lib/even_or_odd.js
@@ -1,5 +1,8 @@
 const evenOrOdd = (number) => {
   // TODO: this should return "even" if the number is even, "odd" otherwise
+  const isEven = (number % 2 === 0);
+  const result = (isEven ? "even" : "odd");
+  return result;
 };
 
 module.exports = evenOrOdd; // Do not remove. We need this for the spec to know about your method.
