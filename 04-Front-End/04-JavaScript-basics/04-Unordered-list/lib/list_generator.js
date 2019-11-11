9f97c1226adc5918ada5323514cb147296d26e7e
Kamrin Klauschie <kamrin.klauschie@gmail.com>
2019-11-11 19:55:40 -0300

    Implemented first listItem method

--- 04-Front-End/04-JavaScript-basics/04-Unordered-list/lib/list_generator.js
+++ 04-Front-End/04-JavaScript-basics/04-Unordered-list/lib/list_generator.js
@@ -1,9 +1,13 @@
 const listItem = (content) => {
   // TODO: return the proper <li> HTML tag with its content (as a string)
+  return `<li class="list-group-item">${content}</li>`;
 };
 
 const unorderedList = (items) => {
   // TODO: return the proper <ul> markup (as a string)
+  items.forEach((item) => {
+    return `<li class="list-group-item">${item}</li>`;
+  });
 };
 
 module.exports = { listItem, unorderedList }; // Do not remove.
