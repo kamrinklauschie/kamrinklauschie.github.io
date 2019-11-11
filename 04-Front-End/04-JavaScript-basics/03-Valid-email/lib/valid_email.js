4d67da894e061a6e52469e29efff7c1b6579ec8d
Kamrin Klauschie <kamrin.klauschie@gmail.com>
2019-11-11 16:04:01 -0300

    Implemented email Regex

--- 04-Front-End/04-JavaScript-basics/03-Valid-email/lib/valid_email.js
+++ 04-Front-End/04-JavaScript-basics/03-Valid-email/lib/valid_email.js
@@ -1,5 +1,7 @@
 const valid = (email) => {
   // TODO: return true if the `email` string has the right pattern!
+  const pattern = (/^[\w+.]+@\w+\.\w{2,}(?:\.\w{2})?$/);
+  return pattern.test(email);
 };
 
 module.exports = valid; // Do not remove.
