9538282ca26c86887b8fffe9b528e4fb71ce70f7
Kamrin Klauschie <kamrin.klauschie@gmail.com>
2019-11-11 13:26:57 -0300

    Implemented sum function

--- 04-Front-End/04-JavaScript-basics/02-Sum-of-negative/lib/sum_of_negative.js
+++ 04-Front-End/04-JavaScript-basics/02-Sum-of-negative/lib/sum_of_negative.js
@@ -1,5 +1,14 @@
 const sumOfNegative = (numbers) => {
   // TODO: You are getting a `numbers` array. Return the sum of **negative** numbers only.
+  const negNumbers = [];
+  numbers.forEach((number) => {
+    if (number < 0) { negNumbers.push(number); }
+  });
+  let sum = 0;
+  negNumbers.forEach((negNumber) => {
+    sum += negNumber;
+  });
+  return sum;
 };
 
 module.exports = sumOfNegative; // Do not remove.
