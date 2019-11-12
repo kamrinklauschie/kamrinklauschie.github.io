d383ba93a8f85885b4a1379159c2957b3591be80
Kamrin Klauschie <kamrin.klauschie@gmail.com>
2019-11-12 17:18:59 -0300

    Implemented event listener

--- 04-Front-End/05-DOM-and-Events/03-My-First-Event-Listener/lib/listener.js
+++ 04-Front-End/05-DOM-and-Events/03-My-First-Event-Listener/lib/listener.js
@@ -1,1 +1,7 @@
 // TODO: React to a click on the button!
+
+const button = document.querySelector('#clickme');
+button.addEventListener('click', (event) => {
+  event.currentTarget.classList.add("disabled");
+  event.currentTarget.innerHTML = "Bingo!";
+});
