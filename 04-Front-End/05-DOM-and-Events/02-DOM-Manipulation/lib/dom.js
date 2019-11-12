59fdabb86c79a61ae77000cc0cea2fba2e9a72a6
Kamrin Klauschie <kamrin.klauschie@gmail.com>
2019-11-12 16:56:48 -0300

    Implemented DOM manipulation

--- 04-Front-End/05-DOM-and-Events/02-DOM-Manipulation/lib/dom.js
+++ 04-Front-End/05-DOM-and-Events/02-DOM-Manipulation/lib/dom.js
@@ -14,58 +14,64 @@ module.exports = function runChallenges(check) {
   //       Make the function getEmail() return it
   const getEmail = () => {
     // TODO: return the email
+    const email = document.querySelector('#email');
+    return email.value;
   };
 
   // /!\ DO NOT CHANGE THIS LINE - TEST PURPOSES
   const borisesEmail = getEmail();
 
   // Ex 2. Change the content of the email input by writing your own email address
-
+  const email = document.querySelector('#email');
+  email.value = "kamrin.klauschie@gmail.com";
 
   // Ex 3. Replace the email hint (next to the input) with 'This is my email now'
   //       The text should be emphasized using a <strong> tag
-
-
+  const emailHint = document.getElementById('email-hint');
+  emailHint.innerHTML = "<strong>This is my email now</strong>";
 
   // Ex 4. Add the .blue CSS class to the th elements
-
-
+  document.querySelectorAll('th').forEach((element) => {
+    element.classList.add("blue");
+  });
 
   // Ex 5. Count the number of table body rows there are
   //       Make the function teamCount() return it
   const teamCount = () => {
     // TODO: return the number of teams
+    const count = document.querySelectorAll('tbody tr').length;
+    return count;
   };
 
   // /!\ DO NOT CHANGE THIS LINE - TEST PURPOSES
   const teamCountBeforeAddition = teamCount();
-
   // Ex 6. Say there is a 15th team added to the table.
   //       Add a row at the bottom, this new team should have zero points.
-
+  const element = document.querySelector('table tbody');
+  element.insertAdjacentHTML('beforeend', "<tr><td>15</td><td>Paris Saint Germaine</td><td>0</td></tr>");
 
 
   // Ex 7. Write some code to sum all points given to all teams
   //       Make the function summarizePoints() return it
   const summarizePoints = () => {
     // TODO: return the sum
+    let sum = 0;
+    const elements = document.querySelectorAll('tbody tr td:nth-child(3)');
+    elements.forEach((point) => {
+      sum += Number.parseInt(point.innerText, 10);
+    });
+    return sum;
   };
 
-
   // Ex 8. Change the background color of all `<th>` cells to #DDF4FF
-
-
+  const cells = document.querySelectorAll('th');
+  cells.forEach((cell) => {
+    cell.style.backgroundColor = "rgb(221, 244, 255)";
+  });
 
   // Ex 9. Remove the "Email:" label from the DOM
-
-
-
-
-
-
-
-
-
+  const label = document.querySelector('label');
+  label.remove();
 
   // Checking exercise answers. DO NOT MODIFY THIS LINE
   check(borisesEmail, teamCountBeforeAddition, summarizePoints());
