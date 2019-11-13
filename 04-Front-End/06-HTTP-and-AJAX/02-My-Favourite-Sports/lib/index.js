ed7b0b55ad166b23d0becce4b2299a21e02eecf0
Kamrin Klauschie <kamrin.klauschie@gmail.com>
2019-11-13 13:22:18 -0300

    Implemented event listeners

--- 04-Front-End/06-HTTP-and-AJAX/02-My-Favourite-Sports/lib/index.js
+++ 04-Front-End/06-HTTP-and-AJAX/02-My-Favourite-Sports/lib/index.js
@@ -1,1 +1,13 @@
 // TODO: write your code here!
+
+// Listen to all squares
+const buttons = document.querySelectorAll(".clickable");
+
+buttons.forEach((button) => {
+// On click
+  button.addEventListener('click', (event) => {
+    const clickable = event.currentTarget;
+    // Make active
+    clickable.classList.toggle('active');
+  });
+});
