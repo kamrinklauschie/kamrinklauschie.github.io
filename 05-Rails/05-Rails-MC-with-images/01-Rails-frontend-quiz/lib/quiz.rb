d7e29eb460ef59170d5d714d78fdd767e6d10dd8
Kamrin Klauschie <kamrin.klauschie@gmail.com>
2019-11-22 11:53:58 -0300

    Finished quiz

--- 05-Rails/05-Rails-MC-with-images/01-Rails-frontend-quiz/lib/quiz.rb
+++ 05-Rails/05-Rails-MC-with-images/01-Rails-frontend-quiz/lib/quiz.rb
@@ -4,14 +4,17 @@
 
 def rails_app_with_wepback
   # TODO: Return a `String` with the write command to create a new rails app called `mister-cocktail` with webpack.
+  return 'rails new mister-cocktail --webpack'
 end
 
 def modern_javascript
   # TODO: Return a `String` of what you have to add to `application.html.erb` if you want to import
   # the javascript file `application.js` that is inside of `app/javascript`
+  return "<%= javascript_pack_tag 'application' %>"
 end
 
 def add_a_npm_package
   # TODO: Return a `String` with the command to type in the terminal to add the npm package `sweetalert`
   # to your rails app.
+  return 'yarn add sweetalert'
 end
